509c322de0bdf8dc53d79ad8fabc6ae565fa9355
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-07-13 19:09:50 -0500

    update header info

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,26 @@
+import Hero from './Hero'
+
 export default function Header() {
   return (
     <header style={{ width: '100%' }} className="flex flex-col items-center">
+      <Hero />
       <h1 style={{ padding: 0, margin: 0 }}>Stevie Trudell</h1>
+      <h2>MUSICIAN</h2>
       <div style={{ width: '50%' }} className="flex justify-around">
         <span>Drums</span>
+        <span>Percussion</span>
         <span>Guitar</span>
         <span>Synth</span>
-        <span>Recording</span>
-        <span>Performing</span>
+        <span>Ableton Live</span>
+        <span>Session Player</span>
+        <span>Performance</span>
+      </div>
+      <h2>AUDIO ENGINEER</h2>
+      <div style={{ width: '50%' }} className="flex justify-around">
+        <span>Studio Recording</span>
+        <span>Live Sound</span>
+        <span>Mixing</span>
+        <span>Composing</span>
         <span>Producing</span>
       </div>
     </header>
